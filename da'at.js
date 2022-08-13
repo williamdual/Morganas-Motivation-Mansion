db0bb48c44a6dcfc80e723674e0a539f182f34d2
williamdual <williamdual115@gmail.com>
2022-08-12 23:21:08 -0400

    improved options page and detailing

--- da'at.js
+++ da'at.js
@@ -1,20 +1,25 @@
 
-function save_theme() {
+function save_options() {
     let theme = document.querySelector('input[name="theme"]:checked').value;
+    let vol = document.getElementById("volume-slider").value
     console.log(theme);
     chrome.storage.sync.set({
-        Theme: theme
+        Theme: theme,
+        Vol: vol
     })
 }
 
-function restore_theme() {
+function restore_options() {
     //sets default in case its first time then pulls data if availible
     chrome.storage.sync.get({
-        Theme: "P5"
+        Theme: "P5",
+        Vol: 70
     }, function (items) {
         document.getElementById(items.Theme).checked = true; //this works cuz the value and the id are the same (this is bad practice but it works for me)
+        document.getElementById("volume-slider").value = items.Vol;
+
     });
 }
 
-document.addEventListener('DOMContentLoaded', restore_theme);
-document.getElementById('save').addEventListener('click', save_theme);
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
--- mona.js
+++ mona.js
@@ -1,29 +1,31 @@
 //alert("hehehehehehe");
 
-//var inits
-//let note_sounds = ["assets/sounds/1note.wav", "assets/sounds/2notes.wav", "assets/sounds/3notes.wav"]
-let note_sounds = ["assets/sounds/NotesFullSoundTW.wav"]
-let note_img = "assets/imgs/yellowNote.png"
-let note_gif = document.getElementById("notes")
-let noteSound = new Audio();
 
-let vol_bar = document.getElementById("volume-slider");
-
-//its lazzy but adding the same property twice increases its chances of being selected
-let morgana_sounds = ["assets/sounds/hmmmmMeow.wav", "assets/sounds/niceGoing.wav", "assets/sounds/niceGoing.wav",
-    "assets/sounds/woahWeCan.wav", "assets/sounds/woahWeCan.wav", "assets/sounds/woahWeCan.wav",
-    "assets/sounds/woahWeCan.wav"]
-let morgona_face = "assets/imgs/monaNutral.png"
-let morgana_imgs = ["assets/imgs/monaGrining.png", "assets/imgs/monaSmileing.png", "assets/imgs/monaStared.png"]
-let monaSound = new Audio(); //1 is default, ie 100%
-
-sounds = [monaSound, noteSound]
 
 document.addEventListener("DOMContentLoaded", function () { //webpage listening
+    //var inits
+    //let note_sounds = ["assets/sounds/1note.wav", "assets/sounds/2notes.wav", "assets/sounds/3notes.wav"]
+    let note_sounds = ["assets/sounds/NotesFullSoundTW.wav"]
+    let note_img = "assets/imgs/yellowNote.png"
+    let note_gif = document.getElementById("notes")
+    let noteSound = new Audio();
+
+    let vol_bar = document.getElementById("volume-slider");
+
+    //its lazzy but adding the same property twice increases its chances of being selected
+    let morgana_sounds = ["assets/sounds/hmmmmMeow.wav", "assets/sounds/niceGoing.wav", "assets/sounds/niceGoing.wav",
+        "assets/sounds/woahWeCan.wav", "assets/sounds/woahWeCan.wav", "assets/sounds/woahWeCan.wav",
+        "assets/sounds/woahWeCan.wav"]
+    let morgona_face = "assets/imgs/monaNutral.png"
+    let morgana_imgs = ["assets/imgs/monaGrining.png", "assets/imgs/monaSmileing.png", "assets/imgs/monaStared.png"]
+    let monaSound = new Audio(); //1 is default, ie 100%
+
+    sounds = [monaSound, noteSound]
+
     vol_bar.value = 70;
     updateVol();
 
-    generateBackground();
+    loadData();
 
     let morgana = document.getElementById("morgana")
     morgana.addEventListener("click", ohWe)
@@ -69,17 +71,18 @@ document.addEventListener("DOMContentLoaded", function () { //webpage listening
 
     }
 
-    function generateBackground() {
+    function loadData() {
         let theme = "none"
         let bg = document.getElementsByClassName("background_container")
         let bgIcons = document.getElementsByClassName("background_logo")
         let path = "none"
-        let text = document.getElementById("title");
+        //let text = document.getElementById("title");
         chrome.storage.sync.get({
-            Theme: "P5"
+            Theme: "P5",
+            Vol: 70
         }, function (items) {
             theme = items.Theme;
-
+            vol_bar.value = items.Vol;
             if (theme == "P3") {
                 bg[0].style.backgroundColor = "#009ACD";
                 path = 'url("assets/imgs/SEES_Gun.svg")';
